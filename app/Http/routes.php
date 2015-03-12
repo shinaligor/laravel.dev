dcc905633c55a720c895ab67f5b5d7d4694178da
igor shinal <ishinal@softjourn.com>
2015-03-12 12:47:13 +0200

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+//Route::get('post', 'PostController@index');
+//Route::get('post/create', 'PostController@create');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+//	'post' => 'PostController',
+]);
+
+Route::resource('post', 'PostController');
+
+Route::get('main', 'MainController@index');
+Route::get('show/{id}', 'ShowController@getPost');
+Route::post('add', 'AddController@getAdd');
+//Route::get('registration', function() { return view('registration'); });
+//
+//Route::post('/registration', 'UserController@register');
+//Route::post('/login/', 'UserController@login');
+//Route::get('registration', function() { return view('registration'); });
+//
+//Route::post('/registration', 'UserController@register');
+//Route::post('/login/', 'UserController@login');
+
+//Route::post('/registration', 'UserController@register');
+//Route::post('/login/', 'UserController@login');
+//https://github.com/shinaligor/laravel.dev.git
+
+
